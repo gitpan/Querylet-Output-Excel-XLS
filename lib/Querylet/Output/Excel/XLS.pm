87e861c0b2d115cd8d11c40a7949114a293bb2b4
Ricardo SIGNES <rjbs@cpan.org>
2014-02-05 03:38:24 +0000

    Import of RJBS/Querylet-Output-Excel-XLS-0.134 from CPAN.
    gitpan-cpan-distribution: Querylet-Output-Excel-XLS
    gitpan-cpan-version:      0.134
    gitpan-cpan-path:         RJBS/Querylet-Output-Excel-XLS-0.134.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Querylet/Output/Excel/XLS.pm
+++ lib/Querylet/Output/Excel/XLS.pm
@@ -1,17 +1,47 @@
 use strict;
 use warnings;
 package Querylet::Output::Excel::XLS;
-{
-  $Querylet::Output::Excel::XLS::VERSION = '0.133';
-}
+$Querylet::Output::Excel::XLS::VERSION = '0.134';
 use parent qw(Querylet::Output);
 # ABSTRACT: output querylet results to an Excel file
 
 use Spreadsheet::WriteExcel;
 
+# =head1 SYNOPSIS
+#
+#  use Querylet;
+#  use Querylet::Output::Excel::XLS;
+#
+#  database: dbi:SQLite2:dbname=cpants.db
+#
+#  query:
+#    SELECT kwalitee.dist,kwalitee.kwalitee
+#    FROM   kwalitee
+#    JOIN   dist ON kwalitee.distid = dist.id
+#    WHERE  dist.author = 'RJBS'
+#    ORDER BY kwalitee.dist;
+#
+#  output format: xls
+#  output file:   cpants.xls
+#
+# =head1 DESCRIPTION
+#
+# This module registers an output handler to produce excel files, using
+# Spreadsheet::WriteExcel.
+#
+# =method default_type
+#
+# The default type for Querylet::Output::Excel::XLS is "xls"
+#
+# =cut
 
 sub default_type { 'xls' }
 
+# =method handler
+#
+# The output handler uses Spreadsheet::WriteExcel to produce an Excel "xls" file.
+#
+# =cut
 
 sub handler      { \&_as_xls }
 sub _as_xls {
@@ -44,13 +74,15 @@ __END__
 
 =pod
 
+=encoding UTF-8
+
 =head1 NAME
 
 Querylet::Output::Excel::XLS - output querylet results to an Excel file
 
 =head1 VERSION
 
-version 0.133
+version 0.134
 
 =head1 SYNOPSIS
 
